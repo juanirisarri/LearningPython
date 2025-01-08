2ac9c62e6672101eec99aeb0549a474154865f18
juan irisarri <juan.irisarri00@gmail.com>
2025-01-08 15:54:33 +0100

    helloWorld in branch juan

--- PildorasInformaticas/First_codes/helloWorld.py
+++ PildorasInformaticas/First_codes/helloWorld.py
@@ -2,7 +2,11 @@
 # Este es un programa simple de "Hello, World!" en Python
 
 def main():
+    
+    print(" ")
     print("Hellooo, World!")
+    print("I am editing branch 'juan'")
+    print(" ")
 
 if __name__ == "__main__":
     main()
