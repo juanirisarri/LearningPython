438210a16c35239737c6092d05cf707b29ad4ea8
juan irisarri <juan.irisarri00@gmail.com>
2025-01-27 16:54:06 +0100

    While

---
+++ PildorasInformaticas/Flow_Control_Statements/statement_While_1.py
@@ -0,0 +1,25 @@
+
+# while CONDITION==true :
+
+
+print("---------------------")
+print(" ")
+
+i = 1
+while i<=10:
+    print("Ejecución " + str(i))
+    i = i + 1
+
+print(" ")
+print("---------------------")
+print(" ")
+
+i = 10
+j = 1
+while i >= 5 and j <= 3 :
+    print("j = " + str(j) + " || i = " + str(i))
+    i = i - 1
+    j = j + 1 
+
+print(" ")
+print("---------------------")
